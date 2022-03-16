6ae23f60706994c0abfe2b724121a5b13abd165e
Kundan <kundan kamble>
2022-03-16 14:18:15 -0700

    [ADDED] LEAP YEAR PROGRAM

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+    class leapyear
+    {
+
+
+        int y;
+        public void readdata()
+        {
+            Console.WriteLine("Enter the Year in Four Digits : ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        public void leap()
+        {
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", y);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", y);
+            }
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            leapyear obj = new leapyear();
+            obj.readdata();
+            obj.leap();
+        }
+    }
+}
