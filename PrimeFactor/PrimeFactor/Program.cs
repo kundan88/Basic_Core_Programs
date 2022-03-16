fb5497fd5249845df8cd4a5609a17f97693c14d5
Kundan <kundan kamble>
2022-03-16 14:30:31 -0700

    [KUNDAN] ADDED PRIME FACTOR PROGRAM

---
+++ PrimeFactor/PrimeFactor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    class Program
+    {
+        int n, i;
+        public void readData()
+        {
+            Console.WriteLine("Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public void PrimeFact()
+        {
+            Console.WriteLine("Factors:");
+            for (i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    
+        public static void Main(string[] args)
+        {
+        Program p=new Program();
+        p.readData();
+        p.PrimeFact();
+        }
+        }
+
