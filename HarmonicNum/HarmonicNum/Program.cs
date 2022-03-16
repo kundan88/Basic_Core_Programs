1392d569211530a0721c5fbb35f162ab5c9c5a7b
Kundan <kundan kamble>
2022-03-16 14:23:58 -0700

    [KUNDAN] ADDED HARMONIC NO. PROGRAM

---
+++ HarmonicNum/HarmonicNum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+	
+	public void Harminic()
+    {
+        float harmonic = 1;
+        int N =2;
+        for (int i = 2; i <= N; i++)
+        {
+            harmonic += (float)1 / i;
+        }
+        Console.WriteLine("harminic num :" +harmonic);
+
+        
+    }
+
+
+
+public static void Main(string[] args)
+    {
+		Program p=new Program();
+        p.Harminic();
+	
+    }
+}
