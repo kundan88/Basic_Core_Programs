59c2daa541c604584160d30f5fd75510a81baedb
Kundan <kundan kamble>
2022-03-16 19:23:13 -0700

    [KUNDAN] ADDED VOWEL AND CONST UPDATED PROGRAM

--- VowelsAndConst/VowelsAndConst/Program.cs
+++ VowelsAndConst/VowelsAndConst/Program.cs
@@ -41,35 +41,8 @@ public class Program
                     break;
             }
         }
-        Console.ReadKey();
-        if (i >= 48 && i <= 57)
-        {
-            Console.Write("You entered a number, Please enter an alpahbet.");
-        }
-        else
-        {
-            switch (ch)
-            {
-                case 'a':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'i':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'o':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'u':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'e':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                default:
-                    Console.WriteLine("The Alphabet is not a vowel");
-                    break;
-            }
-        }
+        
+
         Console.ReadKey();
     }
           public static void Main(string[] args)
