365f2c8c4f40410b720f6c9cdd071c01ef55bdb5
Kundan <kundan kamble>
2022-03-16 19:52:42 -0700

    [KUNDAN] ADDED FLIP COIN PROGRAM

---
+++ CoinFlipp/CoinFlipp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace FlippingCoin
+{
+    class Program
+    {
+        int heads = 0;
+        int tails = 0;
+        int result = 0;
+        public void showData()
+        {
+            Console.WriteLine("Let's flip a Coin");
+            Random Rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                result = Rand.Next(0, 2);
+
+                if (result < 0.5)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+
+            }
+            Console.WriteLine("heads was flipped " + heads);
+            Console.WriteLine("tails was flipped " + tails);
+
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.showData();
+        }
+    }
+}
+      
+            
+            
+
+
+            
